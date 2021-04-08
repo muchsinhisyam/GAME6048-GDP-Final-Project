923cd9f8734b6d31ddb785be34c728f8548b9891
muchsinhisyam <muchsin.ht@gmail.com>
2021-04-08 16:43:26 +0800

    PlayerController, CameraController added & Animation added

---
+++ Assets/CameraController.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public Transform Player;
+
+    private void Update()
+    {
+        transform.position = new Vector3(Player.position.x, Player.position.y, Player.position.z - 5f);
+    }
+}
